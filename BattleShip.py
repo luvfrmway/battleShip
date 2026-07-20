7b6e60bdbb2d09e90cd0e10e7856e7f9a0ab39d5
FAISON <1653759262157002@mil>
2026-07-20 13:44:56 -0400

    added randomized option to board

--- BattleShip.py
+++ BattleShip.py
@@ -2,7 +2,7 @@ import random
 
 
 def create_board():
-    board = []
+    battleBoard = []
 
     for row in range(4):
         board_row = []
@@ -10,8 +10,8 @@ def create_board():
         for column in range(4):
             board_row.append(" ")
 
-        board.append(board_row)
-    return board
+        battleBoard.append(board_row)
+    return battleBoard
     
 def display_board(board):
     print()
@@ -31,48 +31,59 @@ def display_board(board):
     print(" +-----+-----+-----+-----+")
     print()
 
-def randomLo(board):
-    shipLocation = []
-    shiprow = 0
-    shipcol = 0
-    shiprow = random.randint(1,4)
-    shipcol = random.randint(1,4)
-    shipLocation.append(shiprow)
-    shipLocation.append(shipcol)
-
-    if board[shipcol][shiprow] == "":
-        board[shipcol][shiprow].append("ship")
     return
 
-def convertLo (shipLocation):
-    col = None
+def convertLo (shipLocation, location):
+    columnLetter = location[0]
+    row = int(location[2]) - 1
 
-    
+    if columnLetter == "A":
+        column = 0
+    elif columnLetter == "B":
+        column = 1
+    elif columnLetter == "C":
+        column = 2
+    else:
+        column = 3
+    return row, column
 
-        
 
 def get_location(location):
-    while True: 
-        location = (input("Choose where you want to fire (Example A,1):"))
-        while True:
-            if len(location) == 3:
-                if location[0] in "ABCD":
-                    if location[1] == ",":
-                        if location[2] in "1234":
-                            return location
-                        break
-                        
-            print("Invalid location. Enter A, 1 through D, 4.")
-            location = input(
-                "Choose where you want to fire (Example A,1): "
-            ).upper().replace(" "," ")
+    while True:
+        if len(location) == 3:
+            if location[0] in "ABCD":
+                if location[1] == ",":
+                    if location[2] in "1234":
+                        return location
+                    break
+                    
+        print("Invalid location. Enter A, 1 through D, 4.")
+        location = input(
+            "Choose where you want to fire (Example A,1): "
+        ).upper().replace(" "," ")
     
+
+
 if __name__=="__main__":
     print ("---- Battle Ship 1.0 ----")
     print ("")
     
     battleBoard= create_board()
     display_board(battleBoard)
-    ship = randomLo
+    game = True
+
+    locationList = []
+
+    shiprow = 0
+    shipcol = 0
+    shiprow = random.randint(1,4)
+    shipcol = random.randint(1,4)
+
+    if battleBoard[shipcol-1][shiprow-1] == " ":
+        battleBoard[shipcol-1][shiprow-1] = "ship"
     
-    print (battleBoard)
+    while game:
+        location = (input("Choose where you want to fire (Example A,1): "))
+        locationList.append(location)
+        get_location(location)
+        

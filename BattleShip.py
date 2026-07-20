1ad16424d5e66aea29139ef48fc2266575250001
FAISON <1653759262157002@mil>
2026-07-20 09:58:15 -0400

    Formatted some outputs

--- BattleShip.py
+++ BattleShip.py
@@ -28,11 +28,11 @@ def display_board(board):
             print(f"{board[row][column]:^5}", end="")
 
         print("|")
-
+    
     print(" +-----+-----+-----+-----+")
     print()
 
-def randomLo():
+def randomLo(board):
     shipLocation = []
     for x in range(1):
         shiprow = 0
@@ -41,27 +41,30 @@ def randomLo():
         shipcol = random.randint(1,4)
         shipLocation.append(shiprow)
         shipLocation.append(shipcol)
+    
 
         
 
-def get_location():
-    location = input("Enter location (Example A, 1): "
-    ).upper().replace(" "," ")
-
-    while True:
-        if len(location) == 3:
-            if location[0] in "ABCD":
-                if location[1] == ",":
-                    if location[2] in "1234":
-                        return location
-                    
-        print("Invalid location. Enter A, 1 through D, 4.")
-        location = input(
-            "Enter location (Example A,1): "
-        ).upper().replace(" "," ")
-
+def get_location(location):
+    while True: 
+        location = (input("Choose where you want to fire (Example A,1):"))
+        while True:
+            if len(location) == 3:
+                if location[0] in "ABCD":
+                    if location[1] == ",":
+                        if location[2] in "1234":
+                            return location
+                        break
+                        
+            print("Invalid location. Enter A, 1 through D, 4.")
+            location = input(
+                "Choose where you want to fire (Example A,1): "
+            ).upper().replace(" "," ")
+    
 if __name__=="__main__":
     print ("---- Battle Ship 1.0 ----")
     print ("")
-    display_board
     
+    battleBoard= create_board()
+    display_board(battleBoard)
+    get_location()

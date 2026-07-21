50e1c6f807eda495da9dc3229fda7d2dbfb61265
FAISON <1653759262157002@mil>
2026-07-21 08:58:50 -0400

    define grid size, place ships random/manual, ships cannot overlap

--- BattleShip.py
+++ BattleShip.py
@@ -1,14 +1,9 @@
 import random
 
 
-def create_board():
+def create_board(gridSize):
     battleBoard = []
-    while True:
-        try: 
-            gridSize = int(input("Enter the size of your board. (Ex. 1-10): "))
-            break
-        except(ValueError):
-            print ("Invalid input, try again.")
+
 
     for row in range(gridSize):
         board_row = []
@@ -19,9 +14,22 @@ def create_board():
         battleBoard.append(board_row)
     return battleBoard
     
-def display_board(board, gridSize):
+def display_board(battleBoard, gridSize):
     print()
-    print("    A      B      C      D")
+    if gridSize == 4:
+        print("    A      B     C     D")
+    elif gridSize == 5:
+        print("    A      B     C     D     E")
+    elif gridSize == 6:
+        print("    A      B     C     D     E      F")
+    elif gridSize == 7:
+        print("    A      B     C     D     E      F      G")
+    elif gridSize == 8:
+        print("    A      B     C     D     E      F      G      H")
+    elif gridSize == 9:
+        print("    A      B     C     D     E      F      G      H      I")
+    elif gridSize == 10:
+        print("    A      B     C     D     E      F      G      H      I      J")
 
     for row in range(gridSize):
         print(" +-----+-----+-----+-----+")
@@ -30,7 +38,7 @@ def display_board(board, gridSize):
 
         for column in range(gridSize):
             print("|", end="")
-            print(f"{board[row][column]:^5}", end="")
+            print(f"{battleBoard[row][column]:^5}", end="")
 
         print("|")
     
@@ -39,7 +47,7 @@ def display_board(board, gridSize):
 
     return
 
-def convertLo (location):
+def convertLo (location, ):
     columnLetter = location[0]
     row = int(location[2]) - 1
 
@@ -67,33 +75,99 @@ def checkLo (location):
         location = input(
             "Choose where you want to fire (Example A,1): "
         ).upper().replace(" "," ")
-    
 
-    
+def checkPlaceShip (placeShip, gridSize):
+    while True:
+        if len(placeShip) == 3:
+            if placeShip[0] in "ABCD":
+                if placeShip[1] == ",":
+                    if placeShip[2] in "1234":
+                        return placeShip
+                    break
+                    
+        print(f"Invalid location. Enter A, {gridSize} through D, {gridSize}.")
+        placeShip = input(
+            "Choose where you want to fire (Example A,1): "
+        ).upper().replace(" "," ")
+
+def convertPlaceShip (placeShip):
+    columnLetter = placeShip[0]
+    r = int(placeShip[2]) - 1
+
+    if columnLetter == "A":
+        c = 0
+    elif columnLetter == "B":
+        c = 1
+    elif columnLetter == "C":
+        c = 2
+    else:
+        c = 3
+    return r, c    
         
 
 if __name__=="__main__":
-    while True:
-        try:
-            
-    
-    print ("---- Battle Ship 1.0 ----")
+    print ("---- Battle Ship 1.1 ----")
     print ("")
     
-    battleBoard= create_board()
-    display_board(battleBoard)
+    while True:
+        try: 
+            gridSize = int(input("Enter the size of your board. (Ex. 1-10): "))
+            break
+        except(ValueError):
+            print ("Invalid input, try again.")
+    print ("")
+
+    battleBoard = create_board(gridSize)
+    display_board(battleBoard,gridSize)
     game = True
 
+
+    while True:
+        try:
+            shipChoice = (input("Do you want to place ship(s) randomly or manually?(r/m): "))
+            if shipChoice.lower() == "r" or shipChoice.lower() == "m":
+                break
+            else:
+                print ("Invalid input. Enter r/m")
+        except:
+            print ("Invalid input. Try again!")
+    
+    shipAmount = gridSize//2
+
     locationList = []
-
-    shiprow = 0
-    shipcol = 0
-    shiprow = random.randint(1,4)
-    shipcol = random.randint(1,4)
     shiplocation = []
-    shiplocation.append(shiprow-1)
-    shiplocation.append(shipcol-1)
+    if shipChoice.lower() == "r":
+        for x in range (shipAmount):
+            shiprow = 0
+            shipcol = 0
+            shiprow = random.randint(1,4)
+            shipcol = random.randint(1,4)
+            shiplocation.append(shiprow-1)
+            shiplocation.append(shipcol-1)
+    elif shipChoice.lower() == "m":
+        
+        for x in range (shipAmount):
+            while True:
+                placeShip = input("Choose where you want to place ship (Ex. A,1)")
+                checkPlaceShip(placeShip, gridSize)
+                r, c = convertPlaceShip(placeShip)
+                break
+            if len(shiplocation) >= 1:
 
+                for x in (shiplocation):
+                    if r != x :
+                        for d in (shiplocation):
+                            if c != d[1]:
+                                shiplocation.append(placeShip)
+                    else:
+                        print ("Ship has already been placed here. Try again!")
+                        shipAmount += 1
+                        continue
+            else:
+                shiplocation.append(r)
+                shiplocation.append(c)
+
+        
     # if battleBoard[shipcol-1][shiprow-1] == " ":
     #     battleBoard[shipcol-1][shiprow-1] = "ship"
     
@@ -103,9 +177,11 @@ if __name__=="__main__":
         checkLo (location)
         row, col = convertLo(location)
         
-        if [row,col] == shiplocation:
-            print ("you win")
-            game = False
-        else: 
-            battleBoard[row][col] = "x"
-            display_board(battleBoard)
+        for ship in (shiplocation):
+            if [row,col] == ship:
+                print ("you win")
+                game = False
+            else: 
+                battleBoard[row][col] = "x"
+                print ("Incorrect guess. Try again!")
+                display_board(battleBoard)
